e7422b5ce194bc7508c5a9be656e772547c2651e
rafatmaita <2030684@std.hu.edu.jo>
2023-10-03 09:35:54 -0700

    event task

--- app.js
+++ app.js
@@ -1,75 +1,86 @@
 
-function GetUserGender() {
-  let userGender;
-  while (true) {
-    userGender = prompt("Please enter your gender (male or female):").toLowerCase();
+// function GetUserGender() {
+//   let userGender;
+//   while (true) {
+//     userGender = prompt("Please enter your gender (male or female):").toLowerCase();
 
-    if (userGender === "male") {
-      alert("Welcome, Mr. " + userName);
-      break;
-    } else if (userGender === "female") {
-      alert("Welcome, Ms. " + userName);
-      break;
-    } else {
-      alert("Invalid input. Please enter 'male' or 'female'.");
-    }
-  }
-  return userGender;
+//     if (userGender === "male") {
+//       alert("Welcome, Mr. " + userName);
+//       break;
+//     } else if (userGender === "female") {
+//       alert("Welcome, Ms. " + userName);
+//       break;
+//     } else {
+//       alert("Invalid input. Please enter 'male' or 'female'.");
+//     }
+//   }
+//   return userGender;
+// }
+
+
+
+
+
+// let answers = [];
+
+// let userName = prompt("Please enter your name:");
+// answers.push(userName);
+
+// let userTitle = GetUserGender();
+// answers.push(userTitle);
+
+// let Age = prompt("Please enter your Age:");
+// answers.push(Age);
+
+// let drinkType = prompt("Would you like a hot or cold drink?");
+// answers.push(drinkType);
+
+// let drinkName = prompt("Please enter the name of the drink you want:");
+// answers.push(drinkName);
+
+// alert("Your " + drinkType + " " + drinkName + " is getting prepared.");
+
+// let confirmMessage = "Hello, " + userName + "! You ordered a " + drinkType + " " + drinkName + ".";
+// if (confirm(confirmMessage)) {
+//   console.log(confirmMessage);
+// } else {
+//   console.log("User canceled the order.");
+// }
+
+
+// for (let i = 0; i < answers.length; i++) {
+//   console.log("Answer " + (i + 1) + ": " + answers[i]);
+// }
+let userName, Age, userTitle, drinkType, drinkName;
+
+let sub = document.getElementById("sub");
+sub.onclick = function collectDrinkOrder(e) {
+  e.preventDefault();
+  userName = document.querySelector('#name').value;
+  Age = document.querySelector('#age').value;
+  userTitle=document.getElementById("gender");
+  let gender =userTitle.options[userTitle.selectedIndex].text;
+  // userTitle = document.querySelector('input[name="gender"]:checked').value;
+  drinkType=document.getElementById("drink_type");
+  let text =drinkType.options[drinkType.selectedIndex].text;
+  
+  drinkName = document.querySelector('#drink_name').value;
+
+  const div = document.getElementById("divo");
+  let par = document.createElement("p");
+  div.appendChild(par);
+  let ul = document.createElement("ul");
+  div.appendChild(ul);
+  let li1 = document.createElement("li");
+  ul.appendChild(li1);
+  let li2 = document.createElement("li");
+  ul.appendChild(li2);
+  let li3 = document.createElement("li");
+  ul.appendChild(li3);
+
+  par.innerHTML = userName;
+  li1.innerHTML = gender;
+  li2.innerHTML = Age;
+  li3.innerHTML = text + " " + drinkName;
+  document.body.appendChild(div);
 }
-
-
-
-
-
-let answers = [];
-
-let userName = prompt("Please enter your name:");
-answers.push(userName);
-
-let userTitle = GetUserGender();
-answers.push(userTitle);
-
-let Age = prompt("Please enter your Age:");
-answers.push(Age);
-
-let drinkType = prompt("Would you like a hot or cold drink?");
-answers.push(drinkType);
-
-let drinkName = prompt("Please enter the name of the drink you want:");
-answers.push(drinkName);
-
-alert("Your " + drinkType + " " + drinkName + " is getting prepared.");
-
-let confirmMessage = "Hello, " + userName + "! You ordered a " + drinkType + " " + drinkName + ".";
-if (confirm(confirmMessage)) {
-  console.log(confirmMessage);
-} else {
-  console.log("User canceled the order.");
-}
-
-
-for (let i = 0; i < answers.length; i++) {
-  console.log("Answer " + (i + 1) + ": " + answers[i]);
-}
-const div= document.getElementById("divo");
-let par = document.createElement("p");
-div.appendChild(par);
-let ul= document.createElement("ul");
-div.appendChild(ul);
-let li1= document.createElement("li");
-ul.appendChild(li1);
-let li2= document.createElement("li");
-ul.appendChild(li2);
-let li3= document.createElement("li");
-ul.appendChild(li3);
-
-// let li4= document.createElement("li");
-// ul.appendChild(li4);
-
-par.innerHTML=userName;
-li1.innerHTML=userTitle;
-li2.innerHTML=Age;
-li3.innerHTML=drinkType +" "+ drinkName;
-
-
-document.body.appendChild(div);
