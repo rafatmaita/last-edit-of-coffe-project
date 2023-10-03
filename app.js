bad2e7b9cfc2bb9372b695bd1bf23d3d05499757
rafatmaita <2030684@std.hu.edu.jo>
2023-10-03 05:53:36 -0700

    DOM task

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ answers.push(userName);
 let userTitle = GetUserGender();
 answers.push(userTitle);
 
+let Age = prompt("Please enter your Age:");
+answers.push(Age);
+
 let drinkType = prompt("Would you like a hot or cold drink?");
 answers.push(drinkType);
 
@@ -48,3 +51,25 @@ if (confirm(confirmMessage)) {
 for (let i = 0; i < answers.length; i++) {
   console.log("Answer " + (i + 1) + ": " + answers[i]);
 }
+const div= document.getElementById("divo");
+let par = document.createElement("p");
+div.appendChild(par);
+let ul= document.createElement("ul");
+div.appendChild(ul);
+let li1= document.createElement("li");
+ul.appendChild(li1);
+let li2= document.createElement("li");
+ul.appendChild(li2);
+let li3= document.createElement("li");
+ul.appendChild(li3);
+
+// let li4= document.createElement("li");
+// ul.appendChild(li4);
+
+par.innerHTML=userName;
+li1.innerHTML=userTitle;
+li2.innerHTML=Age;
+li3.innerHTML=drinkType +" "+ drinkName;
+
+
+document.body.appendChild(div);
